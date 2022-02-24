9bc94cdbbb730955e9347716cdbfa604af038185
coticoticotty <beruzebuscotty3000@gmail.com>
2022-02-25 08:03:54 +0900

    Edit message

--- amplify-todo-app/amplifyapp/src/App.js
+++ amplify-todo-app/amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          心臓を捧げよ！
         </p>
         <a
           className="App-link"
